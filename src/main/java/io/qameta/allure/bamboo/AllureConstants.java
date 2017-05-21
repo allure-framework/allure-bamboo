62889bf4818e8d21cff334696bb378a822294ebe
Artem Eroshenko <eroshenkoam@qameta.io>
2017-05-21 23:07:53 +0300

    rename variables

--- src/main/java/io/qameta/allure/bamboo/AllureConstants.java
+++ src/main/java/io/qameta/allure/bamboo/AllureConstants.java
@@ -16,20 +16,21 @@ final class AllureConstants {
     /**
      * The directory with allure results relative from build directory.
      */
-    static String ALLURE_CONFIG_RESULTS_PATH = "allureResultDirectory";
+    static String ALLURE_CONFIG_RESULTS_PATH = "custom.allure.config.results.path";
     /**
      * The subdirectory (or subdirectories) to put generated report into.
      */
-    static String ALLURE_CONFIG_REPORT_PATH = "allureReportPathPrefix";
+    static String ALLURE_CONFIG_REPORT_PATH = "custom.allure.config.report.path";
     /**
      * The name of executable.
      */
+    static String ALLURE_CONFIG_EXECUTABLE = "custom.allure.config.executable";
+
     static String ALLURE_BUILD_REPORT_SUCCESS = "custom.allure.build.report.success";
     static String ALLURE_BUILD_REPORT_ARTIFACT_HANDLER = "custom.allure.build.report.artifact.handler";
     static String ALLURE_BUILD_REPORT_FAILURE_DETAILS = "custom.allure.build.report.output";
     static String ALLURE_CONFIG_ENABLED = "custom.allure.config.enabled";
     static String ALLURE_CONFIG_FAILED_ONLY = "custom.allure.config.failed.only";
-    static String ALLURE_CONFIG_EXECUTABLE = "custom.allure.config.executable";
     static String ALLURE_CONFIG_STORAGE_TYPE = "custom.allure.config.storage.type";
     static String ALLURE_CONFIG_DOWNLOAD_ENABLED = "custom.allure.config.download.enabled";
     static String ALLURE_CONFIG_ENABLED_BY_DEFAULT = "custom.allure.config.enabled.default";
--- src/main/resources/templates/editAllureReportTask.ftl
+++ src/main/resources/templates/editAllureReportTask.ftl
@@ -1,7 +1,7 @@
-[@ww.textfield labelKey="allure.result.directory.label" name="allureResultDirectory" required="true"
+[@ww.textfield labelKey="allure.result.directory.label" name="custom.allure.config.results.path" required="true"
   descriptionKey="allure.result.directory.description"/]
 
-[@ww.textfield labelKey="allure.report.path.prefix.label" name="allureReportPathPrefix" required="true"
+[@ww.textfield labelKey="allure.report.path.prefix.label" name="custom.allure.config.report.path" required="true"
   descriptionKey="allure.report.path.prefix.description"/]
 
 [@ww.select cssClass="builderSelectWidget" labelKey="executable.type" name="custom.allure.config.executable" required="true"
