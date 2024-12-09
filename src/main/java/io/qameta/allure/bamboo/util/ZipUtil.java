370a05c92e374b4c9cc51123181e638d3e59094d
Vitalii <vbragin@qameta.io>
2024-12-09 12:18:07 +0300

    spotless

--- src/main/java/io/qameta/allure/bamboo/util/ZipUtil.java
+++ src/main/java/io/qameta/allure/bamboo/util/ZipUtil.java
@@ -36,7 +36,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
-
 public final class ZipUtil {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ZipUtil.class);
